6f915ec2901aee86eedb7bc10045edcb506ce7fb
Bastian Braun <bb207@gmx.de>
2024-02-27 15:54:24 +0100

    Worked on Holograms

--- src/main/java/de/enderkatze/katzcrafttimer/utitlity/Hologram.java
+++ src/main/java/de/enderkatze/katzcrafttimer/utitlity/Hologram.java
@@ -16,84 +16,116 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 public class Hologram {
     @Getter private final Location location;
 
-    @Getter private final String title;
+    @Getter private final ArmorStand title;
+
+    // First line is the title
+    @Getter private List<ArmorStand> lines = new ArrayList<>();
 
     public Hologram(@NotNull Location location, String title) {
         this.location = location;
+        double lineSpacing = -.3;
         if(title == null) {
-            this.title = Objects.requireNonNull(Main.getInstance().getConfig().getString("defaultHologramTitle"));
+            this.title = createLine(this.location.add(0, lineSpacing, 0), true);
+            this.title.setCustomName(Main.getInstance().getConfig().getString("defaultHologramTitle"));
         } else {
-            this.title = title;
+            this.title = createLine(this.location.add(0, lineSpacing, 0), true);
+            this.title.setCustomName(title);
         }
+        lines.add(this.title);
 
-        ArmorStand titleLine = createLine(this.location.add(0, 1.5, 0));
-        titleLine.setCustomName(this.title);
-        lines.add(titleLine);
+        for(int i = 0; i <= 3; i++) {
+            ArmorStand line = this.location.getWorld().spawn(this.location.add(0, lineSpacing * (i+2), 0), ArmorStand.class);
+            lines.add(line);
+        }
 
         saveToFile();
     }
 
-    public Hologram(@NotNull int id) {
+    public Hologram(@NotNull String id) {
 
-        ArmorStand titleLine = getTitleById(id);
+        this.title  = getTitleById(id);
 
-        if(titleLine != null) {
-            this.title = titleLine.getCustomName();
-            this.location = titleLine.getLocation().add(0, -1.5, 0);
+        if(this.title != null) {
+            this.location = title.getLocation().add(0, .3, 0);
+
+            FileConfiguration data = Main.getInstance().getDataConfig();
+            ConfigurationSection hologramSection = data.getConfigurationSection("hologram." + id + ".lines");
+
+            for (String key : hologramSection.getKeys(false)) {
+                String lineId = hologramSection.getString(key);
+
+                ArmorStand line = (ArmorStand) Bukkit.getEntity(UUID.fromString(lineId));
+
+                if (line != null) {
+                    this.lines.add(line);
+                }
+            }
+
         } else {
-            this.title = null;
             this.location = null;
         }
+
     }
 
-
-    private List<ArmorStand> lines = new ArrayList<>();
-
-    private ArmorStand createLine(Location location) {
+    private ArmorStand createLine(Location location, Boolean hasHitbox) {
         ArmorStand line = location.getWorld().spawn(location, ArmorStand.class);
         line.setVisible(false);
         line.setCustomNameVisible(true);
-        line.setMarker(true);
+        line.setMarker(!hasHitbox);
+        line.setInvulnerable(true);
         return line;
     }
 
     private void saveToFile() {
 
-        ArmorStand title = this.lines.get(0);
 
-        FileConfiguration data = Main.getInstance().getDataConfig();
+        if(this.title != null && this.lines.size() > 1) {
 
-        data.set("hologram." + title.getEntityId() + ".name", title.getCustomName());
+            FileConfiguration data = Main.getInstance().getDataConfig();
 
-        for(int i = 1; i < this.lines.size()-1; i++) {
+            data.set("hologram." + this.title.getUniqueId() + ".name", this.title.getCustomName());
 
-            ArmorStand line = this.lines.get(i);
-            data.set("holograms." + title.getEntityId() + ".lines." + i, line.getEntityId());
+            for (int i = 1; i < this.lines.size() - 1; i++) {
+
+                ArmorStand line = this.lines.get(i);
+                data.set("holograms." + this.title.getUniqueId() + ".lines." + i, line.getEntityId());
 
 
+            }
+            Main.getInstance().saveData();
         }
-        Main.getInstance().saveData();
     }
 
     public void removeHologram() {
+        if(this.title != null && !this.lines.isEmpty()) {
+            this.title.remove();
 
+            for(ArmorStand line : this.lines) {
+                line.remove();
+            }
+
+            FileConfiguration data = Main.getInstance().getDataConfig();
+
+            ConfigurationSection hologramsSection = data.getConfigurationSection("holograms");
+
+
+        }
     }
 
-    public ArmorStand getTitleById(int id) {
+    private ArmorStand getTitleById(String id) {
 
         FileConfiguration data = Main.getInstance().getDataConfig();
         ConfigurationSection hologramsSection = data.getConfigurationSection("holograms");
 
-        for (Entity entity : Bukkit.getWorlds().get(0).getLivingEntities()) {
-            if (entity instanceof ArmorStand && entity.getEntityId() == id) {
-                return (ArmorStand) entity;
-            }
+        if(hologramsSection.contains(id)) {
+
+            return (ArmorStand) Bukkit.getEntity(UUID.fromString(id));
         }
-        
         return null;
     }
 }
